1629fb004fdd2ab21efce4c541011726ffb60be6
BartekBP <bartosz.piotr.baranowski@gmail.com>
2018-12-03 19:12:33 +0100

    BLOG-upgrade
    Creating better version on time
    Give href to blog
    Shorter blog posts' summary
    Link to each post and just title

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]+"..."
+    def pub_time_better(self):
+        return self.pub_date.strftime('%a %d %B %Y')
+    def __str__(self):
+        return self.title
